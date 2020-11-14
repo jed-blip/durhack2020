d06fea5c46d56b8e6f844a31c59ed983db05406e
Jenni M <jennifermoulder17@gmail.com>
2020-11-14 17:40:06 +0000

    Update app.js

--- myapp/app.js
+++ myapp/app.js
@@ -1,27 +1,29 @@
-const express = require('express', 4.17.1)
-const app = express()
-const port = 3080
+const express = require('express', 4.17.1);
+const app = express();
+const port = 3080;
 
-var nameArray = []
-var x = 0
-var roomFull = False
+var nameArray = [];
+var peopleCount = 0;
+var roomFull = False;
+var questionsEmpty = False;
 
-app.get('/game', (req, res) => {
-    if ()
-    res.send
-})
-//error checking
+//ask how to get parameters, what to put after slash
 app.post('/', (req, res) => {
     if (name === '') {
-        res.send("Name is empty")
+        res.send("Name is empty");
     }
-    nameArray.push([name, x]);
-    if (x === 3) {
-        roomFull = True
+    nameArray.push([name, peopleCount]);
+    if (peopleCount === 3) {
+        roomFull = True;
     }
-    x += 1;
+    peopleCount += 1;
     res.send(roomFull)
 })
 
-
+app.get('/game', function (req, res) {
+    if (questionArray.length() == 0) {
+        res.send()
+    }
+    question = questionArray[qIndex];
+})
 

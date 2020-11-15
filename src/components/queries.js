17fd6ad8e80d94e86adc1a791cef7af6f78abc30
Jed <jedvirchis@gmail.com>
2020-11-15 02:50:24 +0000

    Fixed compile warnings

--- src/components/queries.js
+++ src/components/queries.js
@@ -1,77 +1,5 @@
 const host='http://ec2-35-177-205-141.eu-west-2.compute.amazonaws.com:3080/';
 
-
-function login(name){
-    // login - POST - queries: name=""
-    fetch(host+"login?name="+name, {
-        headers:{
-            'content-type':'application/json; charset=UTF-8'
-        },
-        method: 'POST',
-    })
-    //.then(data=>{return data.json()})
-    .then(res=>{console.log(res)})
-};
-
-function roomFull(){
-    // /roomfull - GET - return {full: [boolean]}
-    fetch(host+"roomfull", {
-        headers:{
-            'content-type':'application/json; charset=UTF-8'
-        },
-        method: 'GET',
-    })
-    .then(data=>{return data.json()})
-    .then(res=>{console.log(res)})
-};
-function getQuestion(state,setState){
-// /question - GET - return question
-    
-    fetch(host+"question", {
-        headers:{
-            'content-type':'application/json; charset=UTF-8'
-        },
-        method: 'GET',
-    })
-    .then(data=>{return data.text()})
-    .then(res=>{setState({question:res,answer:state.answer,waiting:state.waiting})})
-};
-
-function sendAnswer(answer,name){
-// /sendanswer - POST - queries: answer="", name=""
-    fetch(host+"sendAnswer?answer="+answer+"&name="+name, {
-        headers:{
-            'content-type':'application/json; charset=UTF-8'
-        },
-        method: 'POST',
-    })
-    //.then(data=>{return data.json()})
-    .then(res=>{console.log(res)})
-};
-
-function allAnswered(){
-// allanswered - GET - return {allanswered: [boolean]}
-    fetch(host+"allAnswered", {
-        headers:{
-            'content-type':'application/json; charset=UTF-8'
-        },
-        method: 'GET',
-    })
-    .then(data=>{return data.json()})
-    .then(res=>{console.log(res)})  
-};
-function getAnswers(name){
-// getanswers - GET - queries: name="" - return {[name]: [answer], ....}
-    fetch(host+"getAnswers?name="+name, {
-        headers:{
-            'content-type':'application/json; charset=UTF-8'
-        },
-        method: 'GET',
-    })
-    .then(data=>{return data.json()})
-    .then(res=>{console.log(res)})
-};
-
 function sendScore(name,score){
 // sendscore - POST - queries: name="", score=""
     fetch(host+"sendscore?name="+name+"&score="+score, {
@@ -85,17 +13,4 @@ function sendScore(name,score){
     //.then(data=>{return data.json()})
     .then(res=>{console.log(res)})
 };
-function getScores(){
-   // getscores - GET - return {[name]: [[roundscore, totalscore]], ....}
-   fetch(host+"getScores", {
-    headers:{
-        'content-type':'application/json; charset=UTF-8'
-    },
-    method: 'GET',
-    
-    })
-    .then(data=>{return data.json()})
-    .then(res=>{console.log(res)})
- 
-};
-export {getQuestion, sendScore};
+export {sendScore};
--- src/components/question.js
+++ src/components/question.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React from "react";
 
 import Waiting from "./waiting";
 import Header from './layout/header';
